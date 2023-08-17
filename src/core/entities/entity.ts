5ece83598c00a20f453181ce1521296859e96261
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-08-16 22:52:02 -0300

    feat: ID das entidades

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,7 +1,7 @@
-import { randomUUID } from "node:crypto"
+import { UniqueEntityID } from "./unique-entity-id"
 
 export class Entity<Props> {
-  private _id: string
+  private _id: UniqueEntityID
   protected props: Props
 
 
@@ -11,7 +11,7 @@ export class Entity<Props> {
 
   constructor(props: any, id?: string) {
     this.props = props
-    this._id = id ?? randomUUID()
+    this._id = new UniqueEntityID(id)
   }
 
 
