7611c5171c0ac6f39b48a766eed36d268253a5b8
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-08-24 11:49:44 -0300

    feat: fetch question comments use case

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -15,10 +15,7 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     return answer
   }
 
-  async findManyByQuestionId(
-    questionId: string,
-    { page }: PaginationParams,
-  ): Promise<Answer[]> {
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
     const answers = this.items
       .filter((item) => item.questionId.toString() === questionId)
       .slice((page - 1) * 20, page * 20)
