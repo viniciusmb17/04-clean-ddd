e8360c03079e4ed6a70cd6eac26ff4b45e77f172
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-08-31 10:28:46 -0300

    feat: notification entity

---
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -0,0 +1,48 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+interface NotificationProps {
+  recipientId: UniqueEntityID
+  title: string
+  content: string
+  read?: Date
+  createdAt: Date
+}
+
+export class Notification extends Entity<NotificationProps> {
+  get recipientId() {
+    return this.props.recipientId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get read() {
+    return this.props.read
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  static create(
+    props: Optional<NotificationProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const notification = new Notification(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return notification
+  }
+}
